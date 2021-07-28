f03a4303cc589ac5887b0234ce7b7a3afbde7593
SL <samanthl795@gmail.com>
2021-07-27 20:38:00 -0700

    Add model files

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -2,11 +2,31 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
 
-class ProductTag extends Model {}
+class ProductTag extends Model { }
 
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product', // 'products' refers to table name
+        key: 'id', // 'id' refers to column name in products table
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag', // 'tags' refers to table name
+        key: 'id', // 'id' refers to column name in tags table
+      }
+    }
   },
   {
     sequelize,
