fad803913e37b22344315b69ea94c3ae94bcdd0b
SL <samanthl795@gmail.com>
2021-07-28 18:33:01 -0700

    Add route for PUT /api/categories/:id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -41,8 +41,22 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!categoryData[0]) {
+      res.status(404).json({ message: 'No category with id ' + req.params.id });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
