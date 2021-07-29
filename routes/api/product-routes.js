3d545429da8766c29da0c2a7032f7d0e91fd204a
SL <samanthl795@gmail.com>
2021-07-28 18:06:06 -0700

    Add route for GET /api/products/:id

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -18,9 +18,19 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findOne({ where: { id: req.params.id } });
+    if (!productData) {
+      res.status(404).json({ message: 'Unable to find product with ID ' + req.params.id });
+    } else {
+      res.status(200).json(productData);
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
