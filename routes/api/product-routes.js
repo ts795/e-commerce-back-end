b4277d27c8785f89dc39b54cddd50d867197f721
SL <samanthl795@gmail.com>
2021-07-28 18:14:21 -0700

    Add associated Category and Tag data for GET /api/products/:id

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -22,7 +22,10 @@ router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
   try {
-    const productData = await Product.findOne({ where: { id: req.params.id } });
+    const productData = await Product.findOne({
+      where: { id: req.params.id },
+      include: [{ model: Category }, { model: Tag }]
+    });
     if (!productData) {
       res.status(404).json({ message: 'Unable to find product with ID ' + req.params.id });
     } else {
