166e9b55dbcd15d05d9489abe0e6efef08873c02
SL <samanthl795@gmail.com>
2021-07-28 18:18:48 -0700

    Include associated Product data for GET /api/tags/:id

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -20,7 +20,7 @@ router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
   try {
-    const tagData = await Tag.findOne({ where: { id: req.params.id } });
+    const tagData = await Tag.findOne({ where: { id: req.params.id }, include: [{ model: Product }] });
     if (!tagData) {
       res.status(404).json({ message: 'Unable to find tag with ID ' + req.params.id });
     } else {
