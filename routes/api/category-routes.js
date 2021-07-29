8d4c0842c9cc4926a726acf3ea2df5ec39d6c849
SL <samanthl795@gmail.com>
2021-07-28 18:22:07 -0700

    Add route for GET /api/categories/:id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -16,9 +16,19 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findOne({ where: { id: req.params.id }, include: [{ model: Product }] });
+    if (!categoryData) {
+      res.status(404).json({ message: 'Unable to find category with ID ' + req.params.id });
+    } else {
+      res.status(200).json(categoryData);
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
