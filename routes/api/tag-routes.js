788a912757b9b3374abcf3cc4e8da0e0341318fd
SL <samanthl795@gmail.com>
2021-07-28 18:00:24 -0700

    Expect tag_name in request body for tag PUT and POST routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -34,9 +34,7 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   // create a new tag
   try {
-    const tagData = await Tag.create({
-      tag_name: req.body.name,
-    });
+    const tagData = await Tag.create(req.body);
     res.status(200).json(tagData);
   } catch (err) {
     res.status(400).json(err);
@@ -46,7 +44,7 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
   try {
-    const tagData = await Tag.update({ "tag_name": req.body.name }, {
+    const tagData = await Tag.update(req.body, {
       where: {
         id: req.params.id,
       },
